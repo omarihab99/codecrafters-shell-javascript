d1dd7f1921452f66c61137d522fd6d0fc9e39608
omarihab99 <omar.ihab.mohamed99@gmail.com>
2025-03-15 01:25:59 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,9 +1,29 @@
+const path = require("path");
 const readline = require("readline");
-
+const fs = require("fs");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+function isExecutableFound(command) {
+  const directories = process.env.PATH.split(path.delimiter);
+  for (const dir of directories) {
+    const fullPath = path.join(
+      dir,
+      command + (process.platform === "win32" ? ".exe" : "")
+    );
+    if (fs.existsSync(fullPath)) {
+      return {
+        found: true,
+        path: fullPath,
+      };
+    }
+  }
+  return {
+    found: false,
+    path: "",
+  };
+}
 const commandMapping = {
   exit: () => {
     process.exit(0);
@@ -16,7 +36,9 @@ const commandMapping = {
     if (commandMapping[command]) {
       console.log(`${command} is a shell builtin`);
     } else {
-      console.log(`${command}: not found`);
+      const result = isExecutableFound(command);
+      if (result.found) console.log(`${command} is ${result.path}`);
+      else console.log(`${command}: not found`);
     }
   },
 };
