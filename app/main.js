0dffd5592ef1d7a9e49da7c5a6f3e2fcb60cc96d
omarihab99 <omar.ihab.mohamed99@gmail.com>
2025-03-15 01:08:49 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -11,11 +11,19 @@ const commandMapping = {
   echo: (_, args) => {
     console.log(args.join(" "));
   },
+  type: (_, args) => {
+    const command = args[0];
+    if (commandMapping[command]) {
+      console.log(`${command} is a shell builtin`);
+    } else {
+      console.log(`${command}: not found`);
+    }
+  },
 };
 function prompt() {
   rl.question("$ ", (answer) => {
     const [command, ...args] = answer.split(" ");
-    if (Object.hasOwn(commandMapping, command)) {
+    if (commandMapping[command]) {
       commandMapping[command](command, args);
     } else {
       console.log(`${command}: command not found`);
