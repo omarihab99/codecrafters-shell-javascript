cbb52b9fb9724a06aada13348285e1298805fc94
omarihab99 <omar.ihab.mohamed99@gmail.com>
2025-03-15 00:58:24 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -4,10 +4,23 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const commandMapping = {
+  exit: () => {
+    process.exit(0);
+  },
+  _: (command) => {
+    console.log(`${command}: command not found`);
+    prompt();
+  },
+};
 function prompt() {
   rl.question("$ ", (answer) => {
-    console.log(`${answer}: command not found`);
-    prompt();
+    const command = answer.split(" ")[0];
+    if (Object.hasOwn(commandMapping, command)) {
+      commandMapping[command]();
+    } else {
+      commandMapping._(command);
+    }
   });
 }
 prompt();
